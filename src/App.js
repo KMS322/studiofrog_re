0cf2b0b031704be8f4b45f660c2044dc352ac62b
KMS322 <kms930322@naver.com>
2024-03-22 10:10:05 +0900

    root css add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import "./css/fonts.css";
-import "normalize.css";
 import React, { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
