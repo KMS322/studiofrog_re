6fd7402d9cf7a04c1435c84060ae6cceca33a982
KMS322 <kms930322@naver.com>
2024-03-28 09:02:07 +0900

    update

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3060";
-// export const API_URL = "http://studiofrog.kr:3060";
+// export const API_URL = "http://localhost:3060";
+export const API_URL = "http://studiofrog.kr:3060";
